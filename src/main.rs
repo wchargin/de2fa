63f27f0fdfc25beaadfe4c4a0f94717fd0b23623
William Chargin <wchargin@gmail.com>
2018-05-02 19:48:04 -0700

    Decode an image passed as a command-line argument

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+extern crate image;
+
+use image::GenericImage;
+
 fn main() {
-    println!("Hello, world!");
+    let argv: Vec<_> = std::env::args().collect();
+    if argv.len() <= 1 {
+        eprintln!("Usage: {} IMAGE_FILENAME", argv[0]);
+        std::process::exit(1);
+    }
+    let filename = &argv[1];
+    let img = match image::open(filename) {
+        Err(e) => {
+            println!("Failed to decode: {}", e);
+            std::process::exit(1);
+        }
+        Ok(img) => img,
+    };
+    println!("Successfully decoded. Dimensions: {:?}", img.dimensions());
 }
